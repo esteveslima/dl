55d539d631f60a7e7204ece471dca88f674a0e58
Esteves <esteves_m_lima@hotmail.com>
2023-09-21 18:57:48 -0300

    fix(contracts): fix get contract by id only for owning users (request 1)

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Op } = require('sequelize');
 const { sequelize } = require('./model');
 const { getProfile } = require('./middleware/getProfile');
 
@@ -13,10 +14,21 @@ app.set('models', sequelize.models);
  * @returns contract by id
  */
 app.get('/contracts/:id', getProfile, async (req, res) => {
-  const { Contract } = req.app.get('models');
   const { id } = req.params;
-  const contract = await Contract.findOne({ where: { id } });
+  const profileId = req.profile.id;
+
+  const { Contract } = req.app.get('models');
+  const contract = await Contract.findOne({
+    where: {
+      id,
+      [Op.or]: [
+        { ContractorId: profileId },
+        { ClientId: profileId },
+      ],
+    },
+  });
   if (!contract) return res.status(404).end();
+
   return res.json(contract);
 });
 module.exports = app;
